42237c39b276214f6320f11319af6ca5d0c6660e
Mo74mm3d <mohammedjalqattan@icloud.com>
2022-03-12 10:56:12 +0300

    Done

---
+++ ios-8-cw.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let firstName:String = "Mohammed"
+let birthYear:Int = 2006
+let age:Int = 2022 - birthYear
+
+var eight:String = "8"
+var four:Int = 4
+var twoPointFive:Double = 2.5
+var result = (Double(four) + twoPointFive)
+
+print("Hello my name is \(firstName) and I am \(age) years old.")
